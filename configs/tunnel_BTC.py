94c538df586cbfc7e86ea6c83e0f64f40a107581
binrey <andre.rybin@yandex.ru>
2024-05-30 09:43:57 +0300

    upd tunnel real trading configs

--- configs/tunnel_BTC.py
+++ configs/tunnel_BTC.py
@@ -3,6 +3,6 @@ from configs.tunnel import config
 
 
 config.ticker.test = "BTCUSDT"
-config.lot.test = 0.005
+config.lot.test = 0.015
 config.body_classifier.test.params.ncross.test = 25
 config.trailing_stop_rate.test = 0.007
--- configs/tunnel_ETH.py
+++ configs/tunnel_ETH.py
@@ -3,6 +3,6 @@ from configs.tunnel import config
 
 
 config.ticker.test = "ETHUSDT"
-config.lot.test = 0.1
+config.lot.test = 0.3
 config.body_classifier.test.params.ncross.test = 15
 config.trailing_stop_rate.test = 0.007
