3ae184377ed8d934c0fb9b87173732c1515e0541
andrybin <my@mail.com>
2024-09-15 08:53:13 +0300

    eth tunnel config

--- configs/tunnel_ETH.py
+++ configs/tunnel_ETH.py
@@ -3,10 +3,10 @@ from configs.tunnel import Param, config
 config.ticker.test = "ETHUSDT"
 config.ticksize.test = 0.01
 
-config.wallet.test = 100
+config.wallet.test = 30
 config.leverage.test = 1
 
 config.body_classifier.test.params.ncross.test = 5
 config.trailing_stop_rate.test = 0.003
 config.hist_buffer_size.test = 32
-config.period.test = "M60"
+config.period.test = "M1"
