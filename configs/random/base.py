b15a63a5889cd9ae8ae987c9493c37ef21041f6b
andrybin <my@mail.com>
2025-06-20 23:55:10 +0300

    feature: random expert

---
+++ configs/random/base.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+from common.type import (
+    ConfigType,
+    Symbols,
+    TimePeriod,
+    Vis,
+    VolEstimRule,
+    VolumeControl,
+)
+from common.utils import FeeRate, update_config
+from experts.core.position_control import *
+from experts.random import Random
+
+config = dict(
+    decision_maker=dict(
+        type=Random,
+        seed=42,
+        time_to_wait=10
+    ),
+    sl_processor=dict(
+        type=SLFixed,
+        active=True,
+        percent_value=0.3
+    ),
+    tp_processor=dict(
+        type=TPFromSL,
+        active=False,
+        scale=0
+    ),
+    trailing_stop=dict(
+        type=FixRate,
+        rate=0.04,
+    ),
+
+    name="random",
+    conftype=None,
+    wallet=1000,
+    volume_control=dict(
+        type=VolumeControl,
+        rule=VolEstimRule.DEPOSIT_BASED,
+        deposit_fraction=1,
+    ),
+    leverage=1,
+    close_only_by_stops=False,
+    hist_size=32,
+    tstart=0,
+    tend=None,
+    period=TimePeriod.M1,
+    symbol=Symbols.BTCUSDT,
+    data_type="bybit",
+    fee_rate=FeeRate(0.1, 0.00016),
+    save_backup=False,
+    save_plots=False,
+    vis_events=Vis.ON_DEAL,
+    vis_hist_length=512,
+    visualize=True,
+    run_model_device=None,
+    no_trading_days=set(),
+    close_last_position=True,
+    handle_trade_errors=False
+)
+
+backtest = update_config(
+    config,
+    conftype=ConfigType.BACKTEST,
+    date_start=np.datetime64("2025-06-20T05:30:00"),
+    date_end=np.datetime64("2025-06-24T00:00:00"),
+    eval_buyhold=False,
+    clear_logs=True,
+    log_trades=False,
+)
+
+optimization = update_config(
+    config,
+    conftype=ConfigType.OPTIMIZE,
+    date_start=np.datetime64("2018-01-01T00:00:00"),
+    date_end=np.datetime64("2025-05-01T00:00:00"),
+    clear_logs=False,
+    log_trades=False,
+    eval_buyhold=False,
+    hist_size=[32, 64, 128, 256],
+    trailing_stop={
+        "rate": [0.03, 0.02, 0.01]
+        },
+    decision_maker={
+        "sharpness": [3, 4, 5, 6],
+        }
+    )
+
+bybit = update_config(
+    config,
+    leverage=2,
+    conftype=ConfigType.BYBIT,
+    credentials="bybit_volprof",
+    clear_logs=False,
+    log_trades=True,
+    save_backup=True,
+    handle_trade_errors=True,
+
+)
---
+++ experts/random.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import Any
+
+import numpy as np
+from loguru import logger
+
+from common.type import Side
+from experts.core.decision_maker import DecisionMaker
+
+
+class Random(DecisionMaker):
+    type = "random"
+    
+    class Levels(str, Enum):
+        """Strategy type for HVOL price level detection"""
+        MANUAL = "manual"  # Uses predefined volume distribution bins
+        AUTO = "auto"  # Uses shadow intersection analysis
+
+    def __init__(self, cfg: dict[str, Any]):
+        super().__init__(cfg)
+        self.seed = cfg["seed"]
+        self.time_to_wait = cfg["time_to_wait"]
+        np.random.seed(self.seed)
+        self.timer = 0
+
+    def look_around(self, h) -> DecisionMaker.Response:
+        order_side, target_volume_fraction = None, 1
+
+        if self.timer < self.time_to_wait:
+            self.timer += 1
+            order_side = None
+        else:
+            order_side = np.random.choice([Side.BUY, Side.SELL, None, None])
+            self.timer = 0
+
+        if order_side is not None:
+            self.sl_definer[Side.BUY] = h["Low"].min()
+            self.sl_definer[Side.SELL] = h["High"].max()
+
+        logger.debug(f"order_side: {order_side}")
+        return DecisionMaker.Response(side=order_side, target_volume_fraction=target_volume_fraction)
+
+    def setup_sl(self, side: Side):
+        return self.sl_definer[side]
+
+    def setup_tp(self, side: Side):
+        return self.tp_definer[side]
+    
+    def setup_indicators(self, cfg: dict[str, Any]):
+        return {}
+
+    def update_inner_state(self, h):
+        return super().update_inner_state(h)
+        
