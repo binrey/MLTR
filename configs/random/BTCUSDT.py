ae6bc0dee1037469ea8db17b7cfe482d689cff63
Andrey Rybin <RybinAV6.transport.mos.ru>
2025-07-02 12:46:30 +0300

    upd: configs

---
+++ configs/random/BTCUSDT.py
@@ -0,0 +1,11 @@
+from common.type import Symbols
+from common.utils import update_config
+from .base import backtest, bybit
+
+updates = dict(
+    symbol=Symbols.BTCUSDT,
+    volume_control = {"deposit_fraction": 0.45},
+)
+
+backtest = update_config(backtest, **updates)
+bybit = update_config(bybit, **updates)
