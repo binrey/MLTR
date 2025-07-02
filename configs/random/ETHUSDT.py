b31f1672a35b00b3f710a1f040f41b05977e95a9
Andrey Rybin <RybinAV6.transport.mos.ru>
2025-07-02 12:46:51 +0300

    upd: configs

--- configs/random/ETHUSDT.py
+++ configs/random/ETHUSDT.py
@@ -1,6 +1,6 @@
 from common.type import Symbols
 from common.utils import update_config
-from configs.macross.base import backtest, bybit
+from .base import backtest, bybit
 
 updates = dict(
     symbol=Symbols.ETHUSDT,
