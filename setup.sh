6994a95bd3824bfd3a95d2b935989c9000ee1bad
andrybin <andrybin@yandex.ru>
2025-10-31 00:41:03 +0300

    upd: setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
-apt install python3.12-venv
-python3 -m venv env
+sudo apt install -y python3.12-venv
+python3 -m venv venv
 source env/bin/activate
  
-apt install xvfb
-sudo apt install libxcb-xinerama0 libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxcb-xkb1 libxkbcommon-x11-0
-sudo apt install libqt5gui5 libqt5core5a libqt5dbus5 libx11-xcb1 libfontconfig1 libfreetype6
+sudo apt install -y xvfb
+sudo apt install -y libxcb-xinerama0 libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxcb-xkb1 libxkbcommon-x11-0
+sudo apt install -y libqt5gui5 libqt5core5a libqt5dbus5 libx11-xcb1 libfontconfig1 libfreetype6
 
 pip install -r requirements.txt
