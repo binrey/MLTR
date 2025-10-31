a8f87bc0d5ea1cd222f60bbba14e1dc20dc5d1fc
andrybin <andrybin@yandex.ru>
2025-10-31 16:38:44 +0300

    fix: stop serv

--- deploy/stop_services.sh
+++ deploy/stop_services.sh
@@ -1,7 +1,7 @@
-echo "🚀 Starting volprof@BTCUSDT.service" &&
-sudo systemctl start volprof@BTCUSDT.service &&
+echo "🛑 Stopping volprof@BTCUSDT.service" &&
+sudo systemctl stop volprof@BTCUSDT.service &&
 sudo systemctl --no-pager status volprof@BTCUSDT.service
 
-echo "🚀 Starting volprof@ETHUSDT.service" &&
-sudo systemctl start volprof@ETHUSDT.service &&
+echo "🛑 Stopping volprof@ETHUSDT.service" &&
+sudo systemctl stop volprof@ETHUSDT.service &&
 sudo systemctl --no-pager status volprof@ETHUSDT.service
