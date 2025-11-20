32f22c7d4d216aae2b0c2fd92da99d486f1373c0
andrybin <my@mail.com>
2025-11-21 00:10:22 +0300

    fix: config

--- configs/macross/ETHUSDT.py
+++ configs/macross/ETHUSDT.py
@@ -5,8 +5,8 @@ from configs.macross.base import backtest, bybit
 updates = dict(
     symbol = Symbols.ETHUSDT,
     period=TimePeriod.M60,
-    wallet=4000,
-    volume_control = {"deposit_fraction": 0.25},
+    wallet=1000,
+    volume_control = {"deposit_fraction": 0.5},
     leverage = 1,
     hist_size = 500,
     lot = 0.8,
