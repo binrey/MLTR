e5cb6ca6bac82b93557161671d7872e03f304676
Andrey Rybin <RybinAV6.transport.mos.ru>
2025-12-21 15:38:06 +0300

    upd

--- configs/macross/BTCUSDT.py
+++ configs/macross/BTCUSDT.py
@@ -8,13 +8,13 @@ updates = dict(
     wallet=500,
     volume_control = {"deposit_fraction": 0.5},
     leverage = 1,
-    hist_size = 200,
+    hist_size = 20,
     lot = 1, # 0.16, 0.12 0.1,
     decision_maker = {
-        "ma_fast_period": 40, #16, 8 16,
+        "ma_fast_period": 4, #16, 8 16,
         "upper_levels": 0, #0, 0 3,
         "lower_levels": 1, #100, 100 10,
-        "min_step": 0.9, #0.3 0.3 0.25,
+        "min_step": 0, #0.3 0.3 0.25,
     },
     credentials="bybit_macross",
 )
